33a58a9eed4f59dc01cfeec53cd7c050950d46b3
Jhonathan-Ferroni <ferroni.jhonathan2403@gmail.com>
2026-02-12 17:13:01 -0300

    SellerService and basic FindAll

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -3,7 +3,8 @@ using Microsoft.Extensions.DependencyInjection;
 using SalesWebMvc.Data;
 using System.Configuration;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure; 
-using Pomelo.EntityFrameworkCore.MySql; 
+using Pomelo.EntityFrameworkCore.MySql;
+using SalesWebMvc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddDbContext<SalesWebMvcContext>(options =>
@@ -16,6 +17,7 @@ var builder = WebApplication.CreateBuilder(args);
 //Addscoped
 
 builder.Services.AddScoped<SeedingService>();
+builder.Services.AddScoped<SellerService>();
 
 
 // Add services to the container.
